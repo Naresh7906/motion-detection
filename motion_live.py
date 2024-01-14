3d7f67a5c6f155c89a8ab9d1521ad428cccc6f4c
nareshs-hexaware <112936878+nareshs-hexaware@users.noreply.github.com>
2024-01-14 19:37:52 +0530

    Update motion_live.py

--- motion_live.py
+++ motion_live.py
@@ -1,66 +1,60 @@
 import cv2
-import pafy
 from datetime import datetime
+import yt_dlp
 
 static_back = None
 
-url = "https://www.youtube.com/watch?v=RQA5RcIZlAM"
-video = pafy.new(url)
-best = video.getbest(preftype="mp4")
+url = "https://www.youtube.com/watch?v=1fiF7B6VkCk"
+ydl_opts = {'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'}
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    info_dict = ydl.extract_info(url, download=False)
+    video_url = info_dict.get('url', None)
 
-video = cv2.VideoCapture(best.url)
+video = cv2.VideoCapture(video_url)
 
 curr_frame_time = datetime.now()
 
 fps = 30
 
 while True:
-	itrr_frame_time = datetime.now()
-	check, frame = video.read()
- 
-	motion = 0
+    itrr_frame_time = datetime.now()
+    check, frame = video.read()
 
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    motion = 0
 
-	if static_back is None:
-		static_back = gray
-		continue
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	if (itrr_frame_time-curr_frame_time).total_seconds() >= 0.04:
-		static_back = gray
-		curr_frame_time = datetime.now()
+    if static_back is None:
+        static_back = gray
+        continue
 
-	diff_frame = cv2.absdiff(static_back, gray)
+    if (itrr_frame_time - curr_frame_time).total_seconds() >= 0.04:
+        static_back = gray
+        curr_frame_time = datetime.now()
 
-	thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    diff_frame = cv2.absdiff(static_back, gray)
 
-	#cnts,herch = cv2.findContours(thresh_frame.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	cnts, hierarchy = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
 
-	for contour in cnts:
-		if cv2.contourArea(contour) <100:
-			continue
-		motion = 1
+    cnts, hierarchy = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-		(x, y, w, h) = cv2.boundingRect(contour)
+    for contour in cnts:
+        if cv2.contourArea(contour) < 100:
+            continue
+        motion = 1
 
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        (x, y, w, h) = cv2.boundingRect(contour)
 
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-	cv2.imshow("Gray Frame", gray)
+    cv2.imshow("Gray Frame", gray)
+    cv2.imshow("Difference Frame", diff_frame)
+    cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
-	cv2.imshow("Difference Frame", diff_frame)
-
-	cv2.imshow("Threshold Frame", thresh_frame)
-
-	cv2.imshow("Color Frame", frame)
-
-
-	key = cv2.waitKey(1)
-	if key == ord(' '):
-		break
-
+    key = cv2.waitKey(1)
+    if key == ord(' '):
+        break
 
 video.release()
-
 cv2.destroyAllWindows()
